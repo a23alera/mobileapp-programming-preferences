c6ab537360370cb3bb778ac36a912e1eb7938586
a23alera <a23alera@student.his.se>
2024-05-01 20:52:53 +0200

    Skapade en SecondActivityImplementerade SecondActivity för att tillåta användare att spara data i SharedPreferences med en EditText och tillhandahöll en Button för att återvända till MainActivity

---
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -0,0 +1,43 @@
+package com.example.project;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.SharedPreferences;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        final EditText editText = findViewById(R.id.editTextData);
+        Button saveButton = findViewById(R.id.saveButton);
+        Button backButton = findViewById(R.id.backButton); // Hämta referens till tillbakaknappen
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String textToSave = editText.getText().toString();
+
+                SharedPreferences sharedPreferences = getSharedPreferences("MinaInställningar", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("Nyckel", textToSave);
+                editor.apply();
+
+                Toast.makeText(SecondActivity.this, "Sparat i inställningar", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Avsluta aktiviteten och återgå till MainActivity
+                finish();
+            }
+        });
+    }
+}
